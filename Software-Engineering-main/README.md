acabfc77b3eba1c0acc2d16c654bf131f1bc96e2
Alex-Krysztofiak <128325501+Alex-Krysztofiak@users.noreply.github.com>
2023-03-20 21:31:02 -0400

    Irrelevant code

--- Software-Engineering-main/README.md
+++
@@ -1,3 +0,0 @@
-# Software-Engineering
-Software Engineering group project 
-Alex Krysztofiak, Jason Rosales and Tien Dam.
--- Software-Engineering-main/TipCalc.py
+++
@@ -1,8 +0,0 @@
-print("This program helps you determine the amount to tip")
-bill = float(input("Enter the amount of the bill(e.g. 34.50): "))
-percent = float(input("Enter the tip percentage (e.g. 20 for 20%): "))
-percent = percent/100
-tip = bill*percent
-total = bill + tip
-print("the tip should be {0}".format(tip))
-print("the total (bill plus tip) should be {:.2f}".format(total))
